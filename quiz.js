69b19465fbf129d15f427cffd1cc8086d53b40ce
menacethedenis <denislymer@gmail.com>
2021-03-07 01:44:31 +0000

    created basic quiz functionality

---
+++ quiz.js
@@ -0,0 +1,86 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+ 
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+ 
+    this.questionIndex++;
+}
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+ 
+ 
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+ 
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+ 
+ 
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+ 
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+ 
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+ 
+var questions = [
+    new Question("Who is the patron Saint of Ireland?", ["Saint Britney", "Saint Rick", "Saint Morty", "Saint Patrick"], "Saint Patrick"),
+    new Question("Paddy Losty was a famous Irish:", ["Paintman", "Pintman", "Puntman", "Scientist"], "Pintman"),
+    new Question("GAA stands for:", ["Gaelic Athletic Association", "Grab All Association", "Guinness All Afternoon", "Gone Arseways Association "], "Gaelic Athletic Association"),
+    new Question("Guinness is a:", ["State of mind", "Soft drink", "Stout", "Tourist Trap"], "Stout"),
+    new Question("Leprechauns:", ["Are not real", "Are an insulting propagation of Irish stereotypes", "Have a museum dedicated to them in Dublin", "All of the above"], "All of the above")
+];
+ 
+var quiz = new Quiz(questions);
+
+populate();
